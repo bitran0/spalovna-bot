624436d500d7aad7e69193aa38a53c186092c3ee
bitran0 <40563772+bitran0@users.noreply.github.com>
2018-09-30 15:28:49 +0200

    Add files via upload

---
+++ spalovnabot.py
@@ -0,0 +1,48 @@
+#how did u get dis file
+
+import discord
+from discord import Game
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+import os
+import random
+
+bot = commands.Bot(command_prefix="!")
+
+#Events
+
+@bot.event
+async def on_ready():
+	await bot.change_presence(game=Game(name="Zeman x Ovčáček"))
+	print ("Starting this shit.")
+	print ("user.name: " + bot.user.name)
+	print ("user.id: " + bot.user.id)
+	print ("directory is: " + os.getcwd())
+	print ("--------------------------------------")
+	print ("bot made by: bitran")
+	
+		
+@bot.command(pass_context = True)
+async def info(ctx, target: discord.Member = None):
+	if target is None:
+		target = ctx.message.author
+		await bot.say("Zobrazuji info o: *{}*".format(target))
+		await bot.say("Discord ID: *{}*".format(target.id))
+		await bot.say("Nejvyšší role: *{}*".format(target.top_role))
+		await bot.say("Datum připojení na server: *{}*".format(target.joined_at.strftime("%A, %B %d %Y @ %H:%M:%S %p")))
+		print ("!info given")
+	else:
+		await bot.say("Zobrazuji info o: *{}*".format(target))
+		await bot.say("Discord ID: *{}*".format(target.id))
+		await bot.say("Nejvyšší role: *{}*".format(target.top_role))
+		await bot.say("Datum připojení na server: *{}*".format(target.joined_at.strftime("%A, %B %d %Y @ %H:%M:%S %p")))
+		print ("!info given")
+		
+@bot.command(pass_context = True)
+async def roll(ctx):
+		await bot.say(random.randint(1,100))
+		
+
+bot.run(os.getenv('TOKEN'))
