c5d5517f4302c2f7085ee4c8302fe40d0891073b
bitran0 <40563772+bitran0@users.noreply.github.com>
2018-10-26 22:03:18 +0200

    Update spalovnabot.py

--- spalovnabot.py
+++ spalovnabot.py
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(game=Game(name="Zeman x Ovčáček"))
+	await bot.change_presence(game=Game(name="Solo/Duo with Kvakovich"))
 	print ("Starting this shit. V2")
 	print ("user.name: " + bot.user.name)
 	print ("user.id: " + bot.user.id)
