04022d8dc5141c7c6342500d048147b06f536c74
bitran0 <40563772+bitran0@users.noreply.github.com>
2018-12-20 13:42:10 +0100

    Update spalovnabot.py

--- spalovnabot.py
+++ spalovnabot.py
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(game=Game(name="wasting money on Steam Sales."))
+	await bot.change_presence(game=Game(name="wasting money on Steam."))
 	print ("Starting this shit. V2")
 	print ("user.name: " + bot.user.name)
 	print ("user.id: " + bot.user.id)
